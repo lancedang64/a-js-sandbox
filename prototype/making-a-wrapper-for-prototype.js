a5eef049a19159c3f8d11e404ea8396abdd168d6
Lance Dang <lancedang64@gmail.com>
2021-09-11 11:01:43 +1000

    init sandbox

---
+++ prototype/making-a-wrapper-for-prototype.js
@@ -0,0 +1,21 @@
+// Add to the prototype of all functions the method defer(ms), that returns a wrapper, delaying the call by ms milliseconds.
+// Here’s an example of how it should work:
+// Function.prototype.defer = function (ms) {
+// 	const start = Date.now();
+// 	while (Date.now() - start < ms) {}
+// 	return this;
+// };
+
+Function.prototype.defer = function (ms) {
+	let f = this;
+	return function (...args) {
+		setTimeout(() => f.apply(this, args), ms);
+	};
+};
+
+function f(a, b) {
+	console.log(a + b);
+}
+
+f.defer(1000)(1, 2); // shows 3 after 1 second
+// Please note that the arguments should be passed to the original function.
---
+++ prototype/prototype-vs-__proto__.js
@@ -0,0 +1,55 @@
+// Add to the prototype of all functions the method defer(ms), that runs the function after ms milliseconds.
+// After you do it, such code should work:
+'use strict ';
+
+let f = function () {
+	console.log(`Hello! The f prototype atm is: ${f.__proto__} `);
+};
+
+function defer(ms) {
+	const start = Date.now();
+	while (Date.now() - start < 1000) {}
+	this();
+}
+
+const deferProto = {
+	defer,
+};
+// won't work because these does not exist! (only exit as Object.prototype and so on...)
+// f.prototype.defer = defer;
+// f.prototype = defer;
+//----------------------------------------
+
+// will work
+// set property defer of f prototype (which is Function.prototype) (discouraged unless for polyfilling)
+f.__proto__.defer = defer;
+console.log('f.__proto__.defer = defer');
+console.log(
+	'f.__proto__ === Function.prototype ',
+	f.__proto__ === Function.prototype
+); // true
+console.log('f.__proto__ ', f.__proto__);
+f.defer(1000); // shows "Hello!" after 1 second
+
+f = function () {
+	console.log(`Hello! The f prototype atm is: ${f.__proto__} `);
+};
+// set f prototype to be object deferProto
+f.__proto__ = deferProto;
+console.log('\nf.__proto__ = deferProto');
+console.log(
+	'f.__proto__ === Object.prototype ',
+	f.__proto__ === Object.prototype
+); // false cause should be deferProto
+console.log('f.__proto__ === deferProto ', f.__proto__ === deferProto); // true
+console.log('f.__proto__ ', f.__proto__);
+f.defer(1000); // shows "Hello!" after 1 second
+
+// add to Function.prototype (discouraged unless for polyfilling)
+Function.prototype.defer = function (ms) {
+	setTimeout(this, ms);
+};
+f = function () {
+	console.log(`Hello! The f prototype atm is: ${f.__proto__} `);
+};
+f.defer(1000);
